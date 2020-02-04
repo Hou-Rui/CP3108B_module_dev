5efaebe0d7b087828871f2d13ba0a2c804ace753
Hou-Rui <13244639785@163.com>
2020-02-04 09:47:51 +0800

    add CURVES library

--- code_runner.js
+++ code_runner.js
@@ -3,26 +3,53 @@
 const CodeRunner = (() => {
   const loadedModules = [];
   function loadInputModules() {
-    let moduleDirList = ModuleLoader.getModuleDirList();
+    let moduleDirList;
+    try {
+      moduleDirList = ModuleLoader.getModuleDirList();
+    } catch (errorMessage) {
+      output(errorMessage, true);
+      return;
+    }
     let moduleInput = document.getElementById('module-input');
     let moduleNameList = moduleInput.value.trim().split(' ');
     for (let name of moduleNameList) {
       if (!loadedModules.includes(name)) {
-        ModuleLoader.loadModule(moduleDirList, name);
+        try {
+          ModuleLoader.loadModule(moduleDirList, name);
+          output('Module ' + name + ' is loaded. ', false);
+        } catch (errorMessage) {
+          output(errorMessage, true);
+        }
         loadedModules.push(name);
       }
     }
+  }
+
+  function output(message, isError) { // private
+    let outputDiv = document.getElementById('output-div');
+    let element = document.createElement('p');
+    if (isError) {
+      element.style.color = 'red';
+    }
+    element.appendChild(document.createTextNode(message));
+    outputDiv.appendChild(element);
   }
 
   function execute() {
     let codeTextarea = document.getElementById('code-textarea');
     let outputDiv = document.getElementById('output-div');
     let data = codeTextarea.value;
-    let outputValue = eval(data);
+    let outputValue;
+    try {
+      outputValue = eval(data);
+    } catch (errorMessage) {
+      outputError(errorMessage);
+      return;
+    }
     if (outputValue.$canvas !== undefined) {
       outputDiv.innerHTML = "";
       outputDiv.appendChild(outputValue.$canvas);
-    } else {
+    } else if (outputValue !== undefined) {
       let element = document.createElement("p");
       element.appendChild(document.createTextNode(outputValue));
       outputDiv.appendChild(element);
